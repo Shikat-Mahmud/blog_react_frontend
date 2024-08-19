2bdf2f42111531c039089ca7016e02e861888205
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-08-20 00:58:00 +0600

    project upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Blogs from './components/Blogs';
+import { Route, Routes } from 'react-router-dom';
+import CreateBlog from './components/CreateBlog';
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-dark text-center py-2 shadow-lg">
+        <h1 className='text-white'>React Laravel Blog App</h1>
+      </div>
+      <Routes>
+        <Route path='/' element={<Blogs />} />
+        <Route path='/create' element={<CreateBlog />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { MdOutlineModeEdit } from 'react-icons/md';
+
+const BlogCard = () => {
+    return (
+        <div className="col-12 col-md-2 col-lg-3 my-3">
+            <div className="card border-0 shadow-lg rounded-3">
+                <img src="https://www.nfnoticias.com.br/images/placeholder/600x400.jpg" alt="card image" className='rounded-top-3' />
+                <div className="card-body">
+                    <h2 className="h5"></h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt iste nihil quibusdam, facilis officia explicabo, inventore quae numquam optio illo placeat. Asperiores sint, quos quas alias necessitatibus minus consectetur? Voluptatibus?</p>
+                    <div className="d-flex justify-content-between">
+                        <a href="#" className="btn btn-dark">Details</a>
+                        <a href="#" className="text-dark">
+                            <MdOutlineModeEdit />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogCard
