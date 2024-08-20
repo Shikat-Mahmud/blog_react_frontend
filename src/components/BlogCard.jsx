fd92f5349a5e0d9685c28abe3f284151639e7f2e
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-08-21 00:06:24 +0600

    showing all blogs from api complete

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { MdOutlineModeEdit } from 'react-icons/md';
 
-const BlogCard = () => {
+const BlogCard = ({blog}) => {
+    const showImage = (img) => {
+        return (img) ? "http://127.0.0.1:8000/img/"+img : "https://placehold.co/600x400";
+    }
     return (
         <div className="col-12 col-md-4 col-lg-3 my-3">
             <div className="card border-0 shadow-lg rounded-3">
-                <img src="https://www.nfnoticias.com.br/images/placeholder/600x400.jpg" alt="card image" className='rounded-top-3' />
+                {/* <img src="https://placehold.co/600x400'" alt="card image" className='rounded-top-3' /> */}
+                <img src={showImage(blog.image)} alt="card image" className='rounded-top-3' />
                 <div className="card-body">
-                    <h2 className="h5"></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt iste nihil quibusdam, facilis officia explicabo, inventore quae numquam optio illo placeat. Asperiores sint, quos quas alias necessitatibus minus consectetur? Voluptatibus?</p>
+                    <h2 className="h5">{blog.title}</h2>
+                    <p>{blog.description}</p>
                     <div className="d-flex justify-content-between">
                         <a href="#" className="btn btn-dark">Details</a>
                         <a href="#" className="text-dark">
